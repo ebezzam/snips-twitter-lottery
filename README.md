f6bc3f8a5ecc7ddc8933113bb5483443f13956b1
Eric B <eric.bezzam@epfl.ch>
2018-11-07 18:55:19 +0100

    Adding dummy action code.

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # snips-twitter-lottery
 Randomly chose a winner based on Twitter activity.
+
+Setup
+1. http://adilmoujahid.com/posts/2014/07/twitter-analytics/
+    - make Twitter account, create app, get credentials
+
---
+++ action-twitterLottery.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python2
+from hermes_python.hermes import Hermes
+
+MQTT_IP_ADDR = "localhost"
+MQTT_PORT = 1883
+MQTT_ADDR = "{}:{}".format(MQTT_IP_ADDR, str(MQTT_PORT))
+
+# intents
+INTENT_START = "bezzam:start_game"
+INTENT_WINNER = "bezzam:winner"
+INTENT_STOP = "bezzam:stop_game"
+
+# dict to store participants
+participants = dict()
+
+
+def user_start_game(hermes, intent_message):
+    session_id = intent_message.session_id
+    tts = "Starting game."
+    hermes.publish_continue_session(session_id, tts, [INTENT_STOP])
+
+
+def user_stop_game(hermes, intent_message):
+    session_id = intent_message.session_id
+    tts = "Starting game."
+    hermes.publish_continue_session(session_id, tts, [INTENT_WINNER])
+
+
+def get_winner(hermes, intent_message):
+    session_id = intent_message.session_id
+    tts = "The winner is Eric."
+    hermes.publish_end_session(session_id, tts)
+
+
+with Hermes(MQTT_ADDR) as h:
+    h.subscribe_intent(INTENT_START, user_start_game) \
+     .subscribe_intent(INTENT_STOP, user_stop_game) \
+     .subscribe_intent(INTENT_WINNER, get_winner).start()
