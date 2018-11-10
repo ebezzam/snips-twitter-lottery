6a07ea2dd251c22c42bd7ebe120f01e931667ece
Eric B <eric.bezzam@epfl.ch>
2018-11-10 22:16:51 +0100

    Adding config parser.

--- action-twitterLottery.py
+++ action-twitterLottery.py
@@ -1,10 +1,14 @@
 #!/usr/bin/env python2
+import ConfigParser
 from hermes_python.hermes import Hermes
 import random
 import tweepy
 import threading
-import api_keys
+import io
+import sys
 
+CONFIGURATION_ENCODING_FORMAT = "utf-8"
+CONFIG_INI = "config.ini"
 
 MQTT_IP_ADDR = "localhost"
 MQTT_PORT = 1883
@@ -18,37 +22,40 @@ INTENT_KEEP = "bezzam:keep_collection"
 INTENT_HOW_MANY = "bezzam:how_many_names"
 INTENT_NOT_YET = "bezzam:not_yet"
 
-# target handle and tweet
-twitter_handle = "snips"
-tweet_id = 1060462680597848064  # Trump tweet to test large number of RT
-rt_count = 100
+RT_COUNT = 100      # max is 100
 
-pre_event = ["PatrickBo69", "OttLegalRebels", "Nicochan33"]
+# remove some participants from contention
+pre_event = []
+
+# store participants
+tweet_lottery = dict()
 
 tts_not_gathered = ["Sorry I haven't gathered any participant names.",
                     "I don't have any names.",
                     "Sorry I don't have any usernames.",
                     "Sorry I don't have any gathered names."]
 
-# variables that contains the user credentials to access Twitter API
-access_token = api_keys.access_token
-access_token_secret = api_keys.access_token_secret
-consumer_key = api_keys.consumer_key
-consumer_secret = api_keys.consumer_secret
 
-# authenticate and connect
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_token, access_token_secret)
-api = tweepy.API(auth)
+class SnipsConfigParser(ConfigParser.SafeConfigParser):
+    def to_dict(self):
+        return {section: {option_name : option for option_name, option in self.items(section)} for section in self.sections()}
 
-# store participants
-tweet_lottery = dict()
+
+def read_configuration_file(configuration_file):
+    try:
+        with io.open(configuration_file, encoding=CONFIGURATION_ENCODING_FORMAT) as f:
+            conf_parser = SnipsConfigParser()
+            conf_parser.readfp(f)
+            return conf_parser.to_dict()
+    except (IOError, ConfigParser.Error) as e:
+        return dict()
 
 
 class ExtractRetweetThread(threading.Thread):
 
-    def __init__(self, handle, tweet, rt_count):
+    def __init__(self, api, handle, tweet, rt_count):
         super(ExtractRetweetThread, self).__init__()
+        self.api = api
         self.handle = handle
         self.tweet = tweet
         self.rt_count = rt_count
@@ -59,14 +66,14 @@ class ExtractRetweetThread(threading.Thread):
     def run(self):
 
         # get people who retweeted
-        results = api.retweets(self.tweet, count=self.rt_count)
+        results = self.api.retweets(self.tweet, count=self.rt_count)
         self.rt_screen_names = [r.user.screen_name for r in results]
 
         # check which ones are followers
         for sn in self.rt_screen_names:
             if sn not in pre_event:
-                fship = api.show_friendship(source_screen_name=self.handle,
-                                            target_screen_name=sn)
+                fship = self.api.show_friendship(source_screen_name=self.handle,
+                                                 target_screen_name=sn)
                 if fship[0].followed_by:
                     self.participants.append(str(sn))
 
@@ -97,7 +104,7 @@ def keep_names(hermes, intent_message):
 def collect_names(hermes, intent_message):
     session_id = intent_message.session_id
 
-    tweet_lottery[tweet_id] = ExtractRetweetThread(twitter_handle, tweet_id, rt_count)
+    tweet_lottery[tweet_id] = ExtractRetweetThread(hermes.api, hermes.twitter_handle, hermes.tweet_id, RT_COUNT)
     tweet_lottery[tweet_id].start()
     tts = "I've started collecting the usernames. This may take a couple minutes."
     hermes.publish_end_session(session_id, tts)
@@ -110,11 +117,14 @@ def how_many(hermes, intent_message):
         if tweet_lottery[tweet_id].done:
             participants = tweet_lottery[tweet_id].participants
             n_participants = len(participants)
-            if n_participants > 0:
+            if n_participants > 1:
                 tts = "I've got {} participants. Would you like to know the winner?".format(n_participants)
                 hermes.publish_continue_session(session_id, tts, [INTENT_WINNER, INTENT_NOT_YET])
+            elif n_participants == 1:
+                tts = "I've only got 1 participant. The winner is {}.".format(participants[0])
+                hermes.publish_end_session(session_id, tts)
             else:
-                tts = "There are no participants! Looks like no one wants a maker kit."
+                tts = "There are no participants! Looks like no one wants to win."
                 hermes.publish_end_session(session_id, tts)
         else:
             tts = "Sorry, I haven't finished collecting the participants."
@@ -143,9 +153,6 @@ def get_winner(hermes, intent_message):
                 tweet_lottery[tweet_id].participants.remove(winner)
                 hermes.publish_end_session(session_id, tts)
 
-                # tts = "The winner is {}. Would you like to delete the names?".format(random.choice(participants))
-                # hermes.publish_continue_session(session_id, tts, [INTENT_KEEP, INTENT_DELETE])
-
             else:
                 tts = "There are no participants! Looks like no one wants a maker kit."
                 hermes.publish_end_session(session_id, tts)
@@ -157,11 +164,23 @@ def get_winner(hermes, intent_message):
         hermes.publish_end_session(session_id, tts)
 
 
-with Hermes(MQTT_ADDR) as h:
-    h.subscribe_intent(INTENT_DELETE, delete_names) \
-     .subscribe_intent(INTENT_KEEP, keep_names) \
-     .subscribe_intent(INTENT_NOT_YET, not_yet) \
-     .subscribe_intent(INTENT_HOW_MANY, how_many) \
-     .subscribe_intent(INTENT_COLLECT, collect_names) \
-     .subscribe_intent(INTENT_WINNER, get_winner).start()
+if __name__ == "__main__":
+
+    config = read_configuration_file(CONFIG_INI)
+
+    # authenticate and connect
+    auth = tweepy.OAuthHandler(config["secret"]["consumer_key"], config["secret"]["consumer_secret"])
+    auth.set_access_token(config["secret"]["access_token"], config["secret"]["access_token_secret"])
+    api = tweepy.API(auth)
+
+    with Hermes(MQTT_ADDR) as h:
+        h.api = api
+        h.twitter_handle = config["secret"]["twitter_handle"]
+        h.tweet_id = config["secret"]["tweet_id"]
+        h.subscribe_intent(INTENT_DELETE, delete_names) \
+         .subscribe_intent(INTENT_KEEP, keep_names) \
+         .subscribe_intent(INTENT_NOT_YET, not_yet) \
+         .subscribe_intent(INTENT_HOW_MANY, how_many) \
+         .subscribe_intent(INTENT_COLLECT, collect_names) \
+         .subscribe_intent(INTENT_WINNER, get_winner).start()
 
